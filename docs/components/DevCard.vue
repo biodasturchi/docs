6cab9e7172afbb5fd8bf4802906073ab388204e2
ilosrim <ilosrim@yahoo.com>
2022-12-01 15:39:08 +0500

    edited components & posts

--- docs/components/DevCard.vue
+++ docs/components/DevCard.vue
@@ -1,35 +1,50 @@
 <template>
-  <div id="app" class="card">
-    <div>
-      <h6>{{title}}</h6>
-      <a :href="path">Read More</a>
+  <div class="card" v-for="(item, idx) in items" :key="idx">
+    <div class="cardHead">
+      <small>{{item.topic}}</small>
+      <h6>{{item.title}}</h6>
+      <a :href="item.path">Read More</a>
     </div>
-    <div>
-      <img class="img" :src="src" />
-    </div>
+    <img class="img" :src="item.imgUrl" />
   </div>
 </template>
 
 <script>
 export default {
-  // name: "DevCard",
-  // data() {
-  //   return {
-  //     items: [
-  //       { id: "1", src: "next-markdown.png", tags: ["all", "tag1"] },
-  //       { id: "2", src: "viteblog.png", tags: ["all", "tag2"] },
-  //     ],
-  //   };
-  // },
-  // methods: {
-  //   getImgUrl: function (imagePath) {
-  //     return require('@public/images/bio' + imagePath);
-  //   }
-  // }
-  props: {
-    title: String,
-    path: String,
-    src: String,
+  name: "DevCard",
+  data() {
+    return {
+      items: [
+        {
+          id: 0,
+          title: "Cav1.1 kanaliga ligand sifatida cynaroside moddasining biriktirilish natijalari",
+          path: "./bio/2022-11-25-docking-on-ca-complex",
+          imgUrl: "../public/images/bio/2022-11-25-complex-2.png",
+          topic: "bio",
+        },
+        {
+          id: 0,
+          title: "Ligand docking and binding site analysis with pymol and autodock/vina",
+          path: "./bio/2022-11-29-ligand-docking-and-binding-site-analysis",
+          imgUrl: "../public/images/bio/2022-11-29-banner.png",
+          topic: "bio",
+        },
+        {
+          id: 0,
+          title: "How to build modern docs with vitepress | Full text tutorial",
+          path: "./dev/how-to-build-modern-docs-with-vitepress",
+          imgUrl: "../public/images/dev/viteblog.png",
+          topic: "dev",
+        },
+        {
+          id: 0,
+          title: "Creating a Markdown Blog with NextJS | Full text tutorial",
+          path: "./dev/creating-markdown-blog-with-nextjs",
+          imgUrl: "../public/images/dev/next-markdown.png",
+          topic: "dev",
+        },
+      ]
+    }
   },
 }
 </script>
@@ -44,10 +59,18 @@ export default {
   margin-top: 1rem;
   transition: all 0.3s ease;
 }
+.cardHead {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
 .card:hover {
   border-color: var(--vp-c-brand-light);
 }
 .img {
     width: 200px;
+}
+small {
+  color: #999999;
 }
 </style>
--- docs/posts/index.md
+++ docs/posts/index.md
@@ -8,35 +8,4 @@
 
 # Posts section
 
-## Bioinformatics
-
-<div class="container">
-  <!-- 1 -->
-  <DevCard 
-    title="Cav1.1 kanaliga ligand sifatida cynaroside moddasining biriktirilish natijalari" 
-    path="./bio/2022-11-25-docking-on-ca-complex" 
-    src="../public/images/bio/2022-11-25-complex-2.png" 
-  />
-
-  <!-- 2 -->
-  <DevCard 
-    title="Ligand docking and binding site analysis with pymol and autodock/vina" 
-    path="./bio/2022-11-29-ligand-docking-and-binding-site-analysis" 
-    src="../public/images/bio/2022-11-29-banner.png" 
-  />
-</div>
-
-## Programming
-<div>
-  <DevCard 
-    title="How to build modern docs with vitepress | Full text tutorial" 
-    path="./dev/how-to-build-modern-docs-with-vitepress" 
-    src="../public/images/dev/viteblog.png" 
-  />
-
-  <DevCard 
-    title="Creating a Markdown Blog with NextJS | Full text tutorial" 
-    path="./dev/creating-markdown-blog-with-nextjs" 
-    src="../public/images/dev/next-markdown.png" 
-  />
-</div>
+<DevCard />

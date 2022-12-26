c4d66cd616cbf4208b28a32020824465457c5b52
ilosrim <ilosrim@yahoo.com>
2022-12-26 17:08:09 +0500

    edited blog card component

--- docs/components/BlogCard.vue
+++ docs/components/BlogCard.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="card" v-for="(item, idx) in items" :key="idx">
     <div class="cardHead">
-      <small>{{item.topic}}</small>
-      <h6>{{item.title}}</h6>
+      <small>{{ item.topic }}</small>
+      <h6>{{ item.title }}</h6>
       <a :href="item.path">Read More</a>
     </div>
     <img class="img" :src="item.imgUrl" />
@@ -17,45 +17,51 @@ export default {
       items: [
         {
           id: 0,
-          title: "Cav1.1 kanaliga ligand sifatida cynaroside moddasining biriktirilish natijalari",
+          title:
+            "Cav1.1 kanaliga ligand sifatida cynaroside moddasining biriktirilish natijalari",
           path: "./bio/2022-11-25-docking-on-ca-complex",
-          imgUrl: "https://raw.githubusercontent.com/ilosrim/imgs/master/bio/2022-11-25/complex-2.png",
-          topic: "bio",
+          imgUrl:
+            "https://raw.githubusercontent.com/ilosrim/imgs/master/bio/2022-11-25/complex-2.png",
+          topic: "#bio",
         },
         {
           id: 1,
-          title: "Ligand docking and binding site analysis with pymol and autodock/vina",
+          title:
+            "Ligand docking and binding site analysis with pymol and autodock/vina",
           path: "./bio/2022-11-29-ligand-docking-and-binding-site-analysis",
-          imgUrl: "https://raw.githubusercontent.com/ilosrim/imgs/master/bio/2022-11-29/banner.png",
-          topic: "bio",
+          imgUrl:
+            "https://raw.githubusercontent.com/ilosrim/imgs/master/bio/2022-11-29/banner.png",
+          topic: "#bio",
         },
         {
           id: 2,
           title: "How to build modern docs with vitepress | Full text tutorial",
           path: "./dev/2022-11-22-how-to-build-modern-docs-with-vitepress",
-          imgUrl: "https://raw.githubusercontent.com/ilosrim/imgs/master/dev/viteblog.png",
-          topic: "dev",
+          imgUrl:
+            "https://raw.githubusercontent.com/ilosrim/imgs/master/dev/viteblog.png",
+          topic: "#dev",
         },
         {
           id: 3,
           title: "Creating a Markdown Blog with NextJS | Full text tutorial",
           path: "./dev/2022-11-25-creating-markdown-blog-with-nextjs",
-          imgUrl: "https://raw.githubusercontent.com/ilosrim/imgs/master/dev/nextjs-markdown2.png",
-          topic: "dev",
+          imgUrl:
+            "https://raw.githubusercontent.com/ilosrim/imgs/master/dev/nextjs-markdown2.png",
+          topic: "#dev",
         },
-      ]
-    }
+      ],
+    };
   },
-}
+};
 </script>
 
 <style lang="css" scoped>
 .card {
-  display: flex; 
-  justify-content: space-between; 
-  border: 1px solid var(--vp-c-brand-lighter); 
-  border-radius:5px; 
-  padding: 1rem; 
+  display: flex;
+  justify-content: space-between;
+  border: 1px solid var(--vp-c-brand-lighter);
+  border-radius: 5px;
+  padding: 1rem;
   margin-top: 1rem;
   transition: all 0.3s ease;
 }
@@ -68,7 +74,7 @@ export default {
   border-color: var(--vp-c-brand-light);
 }
 .img {
-    width: 200px;
+  width: 200px;
 }
 small {
   color: #999999;

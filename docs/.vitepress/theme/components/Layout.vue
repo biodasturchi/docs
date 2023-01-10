e7241f13958fed9a4c33199bef4d2ec45ac1f12e
ilosrim <ilosrim@yahoo.com>
2023-01-10 16:51:21 +0500

    Edited Layout & deleted Chat components

--- docs/.vitepress/theme/components/Layout.vue
+++ docs/.vitepress/theme/components/Layout.vue
@@ -3,9 +3,6 @@
     <template #home-features-after>
       <Home v-if="isHome" />
     </template>
-    <template #doc-after>
-      <Chat v-if="isDetail" />
-    </template>
   </Layout>
 </template>
 <script setup>
@@ -13,12 +10,8 @@ import { useData } from "vitepress";
 import { computed } from "vue";
 import DefaultTheme from "vitepress/theme";
 import Home from "./Home.vue";
-import Chat from "./Chat.vue";
 const { Layout } = DefaultTheme;
 const isHome = computed(() => {
   return useData().page.value.relativePath.indexOf("index.md") == -1;
-});
-const isDetail = computed(() => {
-  return useData().page.value.relativePath.indexOf("posts") > -1;
 });
 </script>

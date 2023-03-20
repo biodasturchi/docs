d9e5b9d35864b9884e4175ae80164ea8b6eb8635
mirjr <biodasturchi@gmail.com>
2023-03-20 22:26:48 +0500

    edited config file

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -106,19 +106,19 @@ function sidebarArticles() {
       collapsed: true,
       items: [
         {
-          text: "AlphaFold's new rival? Meta AI predicts shape of 600 million proteins",
+          text: "- AlphaFold's new rival? Meta AI predicts shape of 600 million proteins",
           link: "/posts/2022-11-01-alphafold-s-new-rival",
         },
         {
-          text: "Ligand docking and binding site analysis with pymol and autodock/vina",
+          text: "- Ligand docking and binding site analysis with pymol and autodock/vina",
           link: "/posts/2022-09-29-ligand-docking-and-binding-site-analysis",
         },
         {
-          text: "What's next for AlphaFold and the AI protein-folding revolution",
+          text: "- What's next for AlphaFold and the AI protein-folding revolution",
           link: "/posts/2022-04-13-ai-protein-folding-revolution",
         },
         {
-          text: "DeepMind's AI makes gigantic leap in solving protein structures",
+          text: "- DeepMind's AI makes gigantic leap in solving protein structures",
           link: "/posts/2020-11-30-it-will-change-everything",
         },
       ],
@@ -129,11 +129,11 @@ function sidebarArticles() {
       collapsed: true,
       items: [
         {
-          text: "Creating markdown blog with NextJS",
+          text: "- Creating markdown blog with NextJS",
           link: "/posts/2022-03-07-creating-markdown-blog-with-nextjs",
         },
         {
-          text: "How to build modern docs with vitepress",
+          text: "- How to build modern docs with vitepress",
           link: "/posts/2022-01-05-how-to-build-modern-docs-with-vitepress",
         },
       ],
